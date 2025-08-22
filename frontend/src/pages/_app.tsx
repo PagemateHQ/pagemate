7b63ed27f999488e111a65afde74317b59a07902
Junho Yeo <i@junho.io>
2025-08-23 02:49:34 +0900

    Add font

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import { AppProps } from 'next/app';
 import React from 'react';
 
+import { InstrumentSans } from '@/styles/fonts';
 import '@/styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
       <Component {...pageProps} />
+
+      <style jsx global>{`
+        * {
+          font-family: ${InstrumentSans.style.fontFamily}, system-ui, sans-serif !important;
+          font-optical-sizing: auto;
+        }
+      `}</style>
 
       <div id="portal" />
     </React.Fragment>
---
+++ frontend/src/styles/fonts.ts
@@ -0,0 +1,6 @@
+import { Instrument_Sans } from 'next/font/google';
+
+export const InstrumentSans = Instrument_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
