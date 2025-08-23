e7b85819b8c3f9761a5d3ae692789950f0a73707
Seohun Uhm <uhmseohun@gmail.com>
2025-08-23 15:20:48 +0900

    설마?

--- apps/be/main.py
+++ apps/be/main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         app=app,
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         log_level="debug",
         reload=False,
