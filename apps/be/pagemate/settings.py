9f0782e27d74c007f04c4ea30a74b06dface9d81
Seohun Uhm <uhmseohun@gmail.com>
2025-08-24 03:38:50 +0900

    Invalidate key...

--- apps/be/pagemate/settings.py
+++ apps/be/pagemate/settings.py
@@ -7,8 +7,8 @@ class Settings(BaseSettings):
     mongo_url: str = "mongodb://localhost:27017"
     file_storage_base_path_str: str = "/file-storage"
 
-    openai_api_key: str = "up_0qBatoABAEwg1XX9BIGusjF46qRyj"
-    openai_base_url: str = "https://api.upstage.ai/v1"
+    upstage_api_key: str = "your-upstage-api-key"
+    upstage_base_url: str = "https://api.upstage.ai/v1"
 
     query_embedding_model: str = "embedding-query"
     document_embedding_model: str = "embedding-passage"
